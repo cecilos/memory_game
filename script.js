bb053acb8c00a6fa4f300f13cbb432d6f87c713f
Cecil Osei Wiafe <cecil.wiafe@amalitech.org>
2023-05-13 15:06:31 +0000

    added_some_js_code

---
+++ script.js
@@ -0,0 +1,75 @@
+
+const menuSelect = ()=>{
+    const theme = document.querySelectorAll(".btn-theme");
+    const playerNumber = document.querySelectorAll(".btn-player");
+    const grid = document.querySelectorAll(".btn-grid");
+
+    //set the default state of the data stored in the local browser
+    localStorage.setItem("theme", "numbers");
+    localStorage.setItem("players","1");
+    localStorage.setItem("grid","4x4");
+    //make an array from the selected nodelist for theme
+    Array.from(theme).forEach(active => {
+        active.addEventListener("click",(e)=>{
+            const base = document.getElementById("theme-active");
+            base.id = base.id.replace("theme-active","");
+            active.id += "theme-active";
+            console.log(active);
+
+            if(e.target.value === "icons") {
+                localStorage.setItem("theme","icons");
+                console.log(localStorage);
+            }
+            if(e.target.value === "numbers"){
+                localStorage.setItem("theme","numbers");
+                console.log(localStorage);
+            }
+        })
+        
+        
+    });
+
+    Array.from(playerNumber).forEach(active => {
+        active.addEventListener("click",(e) => {
+            const base = document.getElementById("player-active");
+            base.id = base.id.replace("player-active","");
+            active.id += "player-active";
+
+            switch(e.target.value){
+                case "1":
+                    localStorage.setItem("players", "1");
+                    break;
+                case "2":
+                    localStorage.setItem("players", "2");
+                    //console.log(localStorage)
+                    break;
+                case "3":
+                    localStorage.setItem("players","3");
+                    break;
+                case "4":
+                    localStorage.setItem("players","4");
+                    break;
+            }
+        })
+
+    } );
+
+    Array.from(grid).forEach(active => {
+        active.addEventListener("click",(e)=>{
+            const base = document.getElementById("grid-active");
+            base.id = base.id.replace("grid-active", "");
+            active.id += "grid-active";
+
+            if(e.target.value === "4x4"){
+                localStorage.setItem("grid","6x6");
+                
+            }
+            else{
+                localStorage.setItem("grid","6x6");
+                console.log(localStorage);
+            }
+        })
+    });
+}
+
+menuSelect();
---
+++ theme.js
@@ -0,0 +1,46 @@
+let numbersArray = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,
+10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18];
+
+
+//using a backtick due to the double quotes in the class 
+let iconsArray = [ `<i class="fa-solid fa-envelope"></i>`,
+                `<i class="fa-solid fa-envelope"></i>`,
+                `<i class="fa-solid fa-car"></i>`,
+                `<i class="fa-solid fa-car"></i>`,
+                `<i class="fa-solid fa-droplet"></i>`,
+                `<i class="fa-solid fa-droplet"></i>`,
+                `<i class="fa-solid fa-truck"></i>`,
+                `<i class="fa-solid fa-truck"></i>`,
+                `<i class="fa-solid fa-wifi"></i>`,
+                `<i class="fa-solid fa-wifi"></i>`,
+                `<i class="fa-solid fa-person"></i>`,
+                `<i class="fa-solid fa-person"></i>`,
+                `<i class="fa-solid fa-snowflake"></i>`,
+                `<i class="fa-solid fa-snowflake"></i>`,
+                `<i class="fa-solid fa-shop"></i>`,
+                `<i class="fa-solid fa-shop"></i>`,
+                `<i class="fa-solid fa-anchor"></i>`,
+                `<i class="fa-solid fa-anchor"></i>`,
+                `<i class="fa-solid fa-laptop"></i>`,
+                `<i class="fa-solid fa-laptop"></i>`,
+                `<i class="fa-solid fa-hotel"></i>`,
+                `<i class="fa-solid fa-hotel"></i>`,
+                `<i class="fa-solid fa-walkie-talkie"></i>`,
+                `<i class="fa-solid fa-walkie-talkie"></i>`,
+                `<i class="fa-solid fa-shower"></i>`,
+                `<i class="fa-solid fa-shower"></i>`,
+                `<i class="fa-solid fa-seedling"></i>`,
+                `<i class="fa-solid fa-seedling"></i>`,
+                `<i class="fa-solid fa-file-pdf"></i>`,
+                `<i class="fa-solid fa-file-pdf"></i>`,
+                `<i class="fa-solid fa-hospital"></i>`,
+                `<i class="fa-solid fa-hospital"></i>`,
+                `<i class="fa-solid fa-radio"></i>`,
+                `<i class="fa-solid fa-radio"></i>`,
+                `<i class="fa-solid fa-road"></i>`,
+                `<i class="fa-solid fa-road"></i>`,
+                `<i class="fa-solid fa-soap"></i>`,
+                `<i class="fa-solid fa-soap"></i>`];
+
+
+export {numbersArray,iconsArray};
